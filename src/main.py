8470dfd9dfc4f8098388a15c73678737edd59a8a
jayone.lee <kenbugless@gmail.com>
2026-09-02 14:39:08 +0900

    쿼리 파라미터 정렬 기능 포함한 todos API 추가

---
+++ src/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def health_check_handler():
+    return {"ping": "pong"}
+
+todo_datas = {
+    1: {
+        "id" :1,
+        "contents":"실전! FastAPI 섹션 0 수강",
+        "is_done" : True,
+    },
+    2: {
+        "id" :1,
+        "contents":"실전! FastAPI 섹션 1 수강",
+        "is_done" : False,
+    },
+    3: {
+        "id" :1,
+        "contents":"실전! FastAPI 섹션 2 수강",
+        "is_done" : False,
+    }
+}
+
+@app.get("/todos")
+def get_todos(order:str | None = None ):
+    ret = list(todo_datas.values())
+    if order and order == 'DESC':
+        return ret[::-1]
+    return ret
+
+
